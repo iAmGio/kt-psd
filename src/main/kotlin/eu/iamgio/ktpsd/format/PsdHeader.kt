923d9fb71356cb8f39a86b112b98e8dd1c592c82
iAmGio <giogar2705@gmail.com>
2020-03-28 11:51:45 +0100

    Document PsdHeader properties

--- src/main/kotlin/eu/iamgio/ktpsd/format/PsdHeader.kt
+++ src/main/kotlin/eu/iamgio/ktpsd/format/PsdHeader.kt
@@ -9,13 +9,37 @@ import eu.iamgio.ktpsd.parser.PsdSection
  * @author Giorgio Garofalo
  */
 data class PsdHeader(
+        /**
+         * Signature. Must be 8BPS
+         */
         val signature: String,
+        /**
+         * Version constant. Must be 1
+         */
         val version: Byte,
+        /**
+         * Reserved value. Must be 0
+         */
         private val reserved: Byte,
+        /**
+         * The number of channels in the image, including any alpha channels. Supported range is 1 to 56
+         */
         val channels: Short,
+        /**
+         * The height of the image in pixels. Supported range is 1 to 30,000
+         */
         val height: Int,
+        /**
+         * The width of the image in pixels. Supported range is 1 to 30,000
+         */
         val width: Int,
+        /**
+         * The number of bits per channel. Supported values are 1, 8, 16 and 32
+         */
         val depth: Byte,
+        /**
+         * The color mode of the file. Supported values are: Bitmap = 0; Grayscale = 1; Indexed = 2; RGB = 3; CMYK = 4; Multichannel = 7; Duotone = 8; Lab = 9
+         */
         val colorMode: Byte // TODO make enum
 ) : PsdSection {
 
